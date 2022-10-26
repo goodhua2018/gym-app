0869bc1705f3956f3ef743f09eebdd993fe736ec
Hua <hualyu.2018@gmail.com>
2022-10-26 18:33:00 +1030

    delete exercise

--- src/components/MyPlan.js
+++ src/components/MyPlan.js
@@ -2,6 +2,9 @@ import React from 'react'
 import { useState, useEffect } from 'react'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
+import {RiDeleteBin5Line} from 'react-icons/ri';
+import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 export default function MyPlan() {
   const [loggedInEmail, setLoggedInEmail] = useState('')
@@ -31,37 +34,42 @@ export default function MyPlan() {
     })
   
   }
+  const deleteUserExercise = (itemId) => {
+    fetch(`/api/bakers/${itemId}`, {
+      method: 'DELETE'
+    })
+      .then(() => {
+        setUserGifs(userGifs.filter(t => t.id != itemId))
+    }) 
+  }
   
 
   return (
     <div className="my-plan">
-      <h1>MyPlan</h1>
+      <h1>My Exercise Plan</h1>
       <section>
         <button onClick={() => getPlan()}>click</button>
       </section>
-      <section>
-      {userGifs.map((item, index) => 
-      <div className="exercise-card" key={index}>
-   
-      <Card >
-        <Card.Img variant="top"  src={item.gifUrl} alt={item.name} />
-        <Card.Body>
-          <Button 
-            variant="primary" 
-            style={{marginLeft: '10px'}}
-          >
-            30 min
-          </Button>
-        </Card.Body>
-      </Card>
-    
-    
-      
-      </div>
+      <section className="plan-list">
+        {userGifs.map((item, index) => 
+          <div className="exercise-card" key={index}>
+            <img variant="top"  src={item.gifUrl} alt={item.name} width={'200px'}/>
+            <input 
+              type="text" 
+              className="plan-input"
+              placeholder="min"
+            />
+            <Button 
+              variant="success"
+              size="sm"
+              style={{marginLeft:'2px'}}
+            >
+              Start
+            </Button>
+            <span className={item.id} onClick={() => deleteUserExercise(item.id)}> <RiDeleteBin5Line size={'30'}/></span>
+          </div>
         )}
-
       </section>
-
     </div>
   )
 }
